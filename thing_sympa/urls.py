c64869eed9227e18c5250c0bba5f07378c697898
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-04-08 18:04:04 -0400

    rm cruft

--- thing_sympa/urls.py
+++ thing_sympa/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from thing.urlconf import project_url as purl
 
 urlpatterns = patterns('',
     url('^sympa_config/$', 'thing_sympa.views.sympa_config'),
