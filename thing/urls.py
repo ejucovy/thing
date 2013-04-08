61f24a9c6fbf271bbaff59ff2a662364456a4fba
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-04-08 16:52:02 -0400

    static? please

--- thing/urls.py
+++ thing/urls.py
@@ -101,6 +101,8 @@ urlpatterns += patterns(
 
     
     url(r'^admin/', include(admin.site.urls)),
-    )
 
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': settings.STATIC_ROOT}),
+    )
 
